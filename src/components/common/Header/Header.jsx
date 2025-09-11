90d959e632a1aea61f3cb9fea29313677822d698
Hugo <hquezada@leomail.tamuc.edu>
2025-09-11 12:58:14 -0500

    feat: create Components, context, and pages folders. Create Header component. Add styling.

---
+++ src/components/common/Header/Header.jsx
@@ -0,0 +1,19 @@
+import styles from "./Header.module.css";
+
+const Header = () => {
+  return (
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <h1>Libraea</h1>
+      </div>
+      {/* <div className={styles.search}>
+        <form action="">
+          <input type="text" placeholder="Search for your next adventure..." />
+          <button>O</button>
+        </form>
+      </div> */}
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/pages/Home.jsx
@@ -0,0 +1,11 @@
+import Header from "../components/common/Header/Header.jsx";
+
+const Home = () => {
+  return (
+    <>
+      <Header />
+    </>
+  );
+};
+
+export default Home;
