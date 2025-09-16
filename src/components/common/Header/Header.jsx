77c7a8339afbdeb27199a40a7c489444aadb4e5f
Hugo <hquezada@leomail.tamuc.edu>
2025-09-16 13:28:52 -0500

    feat: create link for navigation in header. Add styling. Move image to the left of the logo.

--- src/components/common/Header/Header.jsx
+++ src/components/common/Header/Header.jsx
@@ -5,11 +5,12 @@ const Header = () => {
   return (
     <header className={styles.header}>
       <div className={styles.logo}>
+        <img src="../../../src/assets/griffin.png" alt="" />
         <h1>Libraea</h1>
       </div>
       <SearchBar />
-      <div className={styles.icons}>
-        <img src="../../../src/assets/kitten.png" alt="" />
+      <div className={styles.link}>
+        <a href="">My Library</a>
       </div>
     </header>
   );
