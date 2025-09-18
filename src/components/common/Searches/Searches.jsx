2f97411dde620c5e05a0dcb70578700cea05e550
Hugo <hquezada@leomail.tamuc.edu>
2025-09-18 13:28:47 -0500

    feat: create handleClick function to search books based on the search query.

--- src/components/common/Searches/Searches.jsx
+++ src/components/common/Searches/Searches.jsx
@@ -1,7 +1,26 @@
+import { useSearch } from "../../../hooks/useSearch";
 import styles from "./Searches.module.css";
+import { ArrowRight } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const Searches = ({ text }) => {
-  return <p className={styles.search}>{text}</p>;
+  const { searchBooks } = useSearch();
+  const navigate = useNavigate();
+
+  const handleOnClick = async (e) => {
+    e.preventDefault();
+    if (text.trim()) {
+      await searchBooks(text);
+      navigate("/search");
+    }
+  };
+
+  return (
+    <button className={styles.search} onClick={handleOnClick}>
+      {text}
+      <ArrowRight />
+    </button>
+  );
 };
 
 export default Searches;
