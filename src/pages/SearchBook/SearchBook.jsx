94356e3aad2133ccc94b67dbb7fb2df0816dcd8d
Hugo <hquezada@leomail.tamuc.edu>
2025-09-19 11:06:57 -0500

    chore: remove unnecessary code from SearchBook component.

--- src/pages/SearchBook/SearchBook.jsx
+++ src/pages/SearchBook/SearchBook.jsx
@@ -1,11 +1,9 @@
 import { useSearch } from "../../hooks/useSearch";
 import styles from "./SearchBook.module.css";
 import BookCard from "../../components/common/BookCard/BookCard";
-import { useBooks } from "../../hooks/useBooks";
 import { ScrollRestoration } from "react-router-dom";
 
 const SearchBook = () => {
-  const { clearList, favorites, wantToRead, recentlySearched } = useBooks();
   const { searchResults, loading, error, hasSearched } = useSearch();
   if (loading)
     return (
@@ -24,27 +22,12 @@ const SearchBook = () => {
   if (searchResults.length === 0)
     return <div className={styles.notFound}>No books found.</div>;
 
-  const handleClear = (listName) => {
-    clearList(listName);
-  };
-  function booklist() {
-    console.log(favorites);
-    console.log(wantToRead);
-    console.log(recentlySearched);
-  }
-  booklist();
-  console.log(searchResults);
   return (
     <div className={styles.results}>
       <ScrollRestoration />
       {searchResults.map((book) => {
         return <BookCard key={book.key} book={book} />;
       })}
-      <button onClick={() => handleClear("favorites")}>clear fav</button>
-      <button onClick={() => handleClear("wantToRead")}>clear saved</button>
-      <button onClick={() => handleClear("recentlySearched")}>
-        clear searched
-      </button>
     </div>
   );
 };
